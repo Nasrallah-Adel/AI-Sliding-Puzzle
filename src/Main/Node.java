fda6cfe3ca42f0c225fccbae624c8285bb4108a0
Nasrallah-Adel <naseradel5@yahoo.com>
2017-05-04 05:54:06 +0200

    Nasrallah

---
+++ src/Main/Node.java
@@ -0,0 +1,18 @@
+package Main;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nasrallah
+ */
+public class Node {
+
+    int n[][] = new int[3][3];
+    int cost;
+    boolean opend;
+}
---
+++ src/Main/Solver.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.util.List;
+
+/**
+ *
+ * @author nasrallah
+ */
+public class Solver {
+public List<Node> solve(){
+    List <Node> childs;
+    //position of zero
+    
+}
+    public static void main(String[] args) {
+        Node start = new Node();
+        Node goal = new Node();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                start.n[i][j] = (int) (Math.random() * 9);
+                for (int a = 0; a <= i; a++) {
+                    for (int b = 0; b < 3; b++) {
+                        if (b == j && a == i) {
+                            break;
+                        }
+                        if (start.n[i][j] == start.n[a][b]) {
+                            j = b;
+                            i = a;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(start.n[i][j] + " ");
+
+            }
+            System.out.println("");
+
+        }
+
+    }
+}
